50c2e899fe23b87ec604274d3406a079d0910e84
juanB <juancruzberrios73@gmail.com>
2025-06-27 13:09:01 -0300

    cambios estilos

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Button, StyleSheet, Alert } from 'react-native';
+import { View, Button, StyleSheet, Alert,Text, Image, TouchableOpacity } from 'react-native';
 import * as SunmiPrinterLibrary from '@mitsuharu/react-native-sunmi-printer-library'
+import { TopBar } from './src/components/top-bar/TopBar';
 
 export default function App() {
-  const handlePrint = async () => {
+  const App = async () => {
     try {
       await SunmiPrinterLibrary.prepare()
       await SunmiPrinterLibrary.printText('Hello from Sunmi V2s!\n');
@@ -20,16 +21,72 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Button title="Print" onPress={handlePrint} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 16,
-  },
-});
+    <>
+      <TopBar/>
+      <View style={styles.container}>
+          <View style={styles.centerContent}>
+            <Image
+              source={require('./src/assets/logoNoLetter.jpeg')} // Asegúrate de tener el logo en esta ruta
+              style={styles.logo}
+              resizeMode="contain"
+            />
+            <Text style={styles.title}>LAS CONDES</Text>
+            <Text style={styles.subtitle}>MEJOR PARA TODOS</Text>
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>INGRESA</Text>
+          </TouchableOpacity>
+        </View>
+    </>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      //justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingVertical: 40,
+    },
+    centerContent: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logo: {
+      width: 90,
+      height: 90,
+      marginBottom: 24,
+    },
+    title: {
+      fontSize: 24,
+      color: '#1976D2',
+      fontWeight: 'bold',
+      letterSpacing: 2,
+      textAlign: 'center',
+    },
+    subtitle: {
+      fontSize: 14,
+      color: '#1976D2',
+      letterSpacing: 1,
+      textAlign: 'center',
+      marginTop: 4,
+    },
+    button: {
+      backgroundColor: '#1976D2',
+      borderRadius: 20,
+      paddingVertical: 12,
+      paddingHorizontal: 40,
+      alignSelf: 'center',
+      marginBottom: 20,
+      width: 180,
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 16,
+      textAlign: 'center',
+      fontWeight: 'bold',
+      letterSpacing: 1,
+    },
+  });
---
+++ src/components/top-bar/TopBar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Image, StyleSheet, Alert, Text, TouchableOpacity  } from 'react-native';
+import { Dimensions } from 'react-native';
+
+export const TopBar = () => {
+  return (
+      <View style={styles.container}>
+        <Image
+            source={require('../../assets/logoNoLetter.jpeg')}
+            style={{ width: 50, height: 50 }}
+        />
+
+      </View>
+    );
+}
+
+const { width } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+    backgroundColor:"#1664C0",
+    padding: 10,
+    alignSelf: 'stretch',
+    height:height * 0.1
+  },
+});
