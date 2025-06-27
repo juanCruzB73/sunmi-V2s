7fe997b988f53aa2759df4d3879a20a6f79b0ed9
juanB <juancruzberrios73@gmail.com>
2025-06-27 12:31:43 -0300

    full screens

---
+++ src/utlis/print.ts
@@ -0,0 +1,17 @@
+import * as SunmiPrinterLibrary from '@mitsuharu/react-native-sunmi-printer-library';
+import { Alert } from 'react-native';
+export const handlePrint = async () => {
+    try {
+      await SunmiPrinterLibrary.prepare()
+      await SunmiPrinterLibrary.printText('Hello from Sunmi V2s!\n');
+      await SunmiPrinterLibrary.printText('This is a test receipt.\n\n\n');
+      Alert.alert('Success', 'Printed successfully');
+    } catch (err) {
+      let errorMessage = 'Failed to print';
+      if (err instanceof Error) {
+        errorMessage = err.message;
+      }
+      console.error(err);
+      Alert.alert('Error', errorMessage);
+    }
+  };
