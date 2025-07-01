a097fd96f36506ea1cd2ca35cdb85c3550c557e2
juanB <juancruzberrios73@gmail.com>
2025-07-01 09:16:06 -0300

    cambios estilos

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,32 +1,48 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
 import ProfileHeader from '../../components/profile/ProfileHeader';
 import ProfileOption from '../../components/profile/ProfileOption';
 import LogoutButton from '../../components/profile/LogoutButton';
+import { TopBar } from '../../components/top-bar/TopBar';
 
 const ProfileScreen: React.FC = () => {
   return (
+    <>
+    <TopBar/>
     <View style={styles.container}>
       <ProfileHeader
         name="Pepe Rosa"
         email="pepe.rosa@lascondes.com"
       />
-
-      <ProfileOption icon="bell" label="Notificaciones" onPress={() => {}} />
-      <ProfileOption icon="question-circle" label="Ayuda" onPress={() => {}} />
-      <ProfileOption icon="cog" label="Configuraciones" onPress={() => {}} />
-
+      <View style={styles.optionContainer}>
+        <ProfileOption icon="bell" label="Notificaciones" onPress={() => {}} />
+        <ProfileOption icon="question-circle" label="Ayuda" onPress={() => {}} />
+        <ProfileOption icon="cog" label="Configuraciones" onPress={() => {}} />
+      </View>
       <LogoutButton onPress={() => {}} />
     </View>
+    </>
   );
 };
 
+const { width } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   container: {
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
     flex: 1,
     backgroundColor: '#fff',
-    padding: 24,
+    width:width,
+    height:height * 0.8
   },
+  optionContainer:{
+    width:width * 0.7,
+    display:"flex",
+   
+  }
 });
 
 export default ProfileScreen;
